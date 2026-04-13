e1d6cd462fc314b523ce82d96e70f5a68ab39163
spelldeed <dighajain1912@gmail.com>
2026-04-13 22:20:58 +0530

    small-error-2

--- src/data.py
+++ src/data.py
@@ -9,7 +9,7 @@ def get_dataloaders(config):
     batch_size = config['dataloader']['batch_size']
     num_workers = config['dataloader']['num_workers']
 
-    transform = transforms.Compose([transforms.toTensor()])
+    transform = transforms.Compose([transforms.ToTensor()])
 
     train_dataset = torchvision.datasets.CIFAR10(root = root, train = True, download = True, transform = transform)
     test_dataset = torchvision.datasets.CIFAR10(root = root, train = False, download = True, transform = transform)
